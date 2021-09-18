21abeed29297e5990654c2c136b78eb2ec8eb7f3
asdf <wnduf0405_@naver.com>
2021-09-18 14:52:05 +0900

    ⚡️ :: Test Redis Connection
    Test Redis Connection

--- src/main/java/com/asdf148/javaproject/global/RedisConfig.java
+++ src/main/java/com/asdf148/javaproject/global/RedisConfig.java
@@ -10,7 +10,7 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory("3.36.224.130", 6379);
     }
 
     @Bean
