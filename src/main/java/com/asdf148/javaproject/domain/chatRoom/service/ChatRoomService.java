4637ac0427dcb968b71b97116cad77678e410456
jhhong0509 <jhhong0509@naver.com>
2021-10-14 14:32:26 +0900

    ⚡️ :: add getChatRoomList

--- src/main/java/com/asdf148/javaproject/domain/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/asdf148/javaproject/domain/chatRoom/service/ChatRoomService.java
@@ -3,6 +3,8 @@ package com.asdf148.javaproject.domain.chatRoom.service;
 import com.asdf148.javaproject.domain.auth.entity.UserRepository;
 import com.asdf148.javaproject.domain.chatRoom.entity.ChatRoom;
 import com.asdf148.javaproject.domain.chatRoom.entity.ChatRoomRepository;
+import com.asdf148.javaproject.domain.chatRoom.payload.ChatRoomListResponse;
+import com.asdf148.javaproject.domain.chatRoom.payload.ChatRoomResponse;
 import com.asdf148.javaproject.domain.project.entity.Project;
 import com.asdf148.javaproject.domain.project.entity.ProjectRepository;
 import com.asdf148.javaproject.global.config.JwtToken;
@@ -12,6 +14,7 @@ import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,19 @@ public class ChatRoomService {
     private final ProjectRepository projectRepository;
     private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
+
+    public ChatRoomListResponse getProjectChatRooms(ObjectId projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow();
+
+        return new ChatRoomListResponse(project.getChatRooms()
+                .stream().map(chatRoom -> ChatRoomResponse.builder()
+                        .chatRoomImage(chatRoom.getRoomImage())
+                        .chatRoomName(chatRoom.getName())
+                        .id(chatRoom.getId().toString())
+                        .build())
+                .collect(Collectors.toList()));
+    }
 
     public void initialChatRoom(String token, ObjectId projectId) {
         TokenContent tokenContext = jwtToken.decodeToken(token);
