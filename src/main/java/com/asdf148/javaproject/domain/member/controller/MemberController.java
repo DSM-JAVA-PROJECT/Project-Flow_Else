7b06e579cba4a2dd8ff063855669fa88eb1a4cb1
jhhong0509 <jhhong0509@naver.com>
2021-10-14 20:15:07 +0900

    :bug: :: token 수정

--- src/main/java/com/asdf148/javaproject/domain/member/controller/MemberController.java
+++ src/main/java/com/asdf148/javaproject/domain/member/controller/MemberController.java
@@ -2,12 +2,10 @@ package com.asdf148.javaproject.domain.member.controller;
 
 import com.asdf148.javaproject.domain.member.payload.ProjectMemberListResponse;
 import com.asdf148.javaproject.domain.member.service.MemberService;
+import com.asdf148.javaproject.global.config.JwtToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -17,15 +15,16 @@ import java.security.Principal;
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtToken jwtToken;
 
     @GetMapping("/member")
-    public ProjectMemberListResponse getMemberList(@AuthenticationPrincipal Principal principal, @PathVariable String projectId) {
-        return memberService.getMemberList(principal.getName(), projectId);
+    public ProjectMemberListResponse getMemberList(@RequestHeader("Authorization") String token, @PathVariable String projectId) {
+        return memberService.getMemberList(jwtToken.decodeToken(token.substring(7)).getEmail(), projectId);
     }
 
     @GetMapping("/member/{chatRoomId}")
-    public ProjectMemberListResponse getNotParticipatedMemberList(@AuthenticationPrincipal Principal principal, @PathVariable String projectId, @PathVariable String chatRoomId) {
-        return memberService.getNotParticipatedMemberList(principal.getName(), projectId, chatRoomId);
+    public ProjectMemberListResponse getNotParticipatedMemberList(@RequestHeader("Authorization") String token, @PathVariable String projectId, @PathVariable String chatRoomId) {
+        return memberService.getNotParticipatedMemberList(jwtToken.decodeToken(token.substring(7)).getEmail(), projectId, chatRoomId);
     }
 
 }
