2d0c0279178c0bc290ca9f9c7405d0dbb2efb0cd
jhhong0509 <jhhong0509@naver.com>
2021-10-15 09:23:39 +0900

    ⚡️ :: SecretKey Base64 Encoding

--- src/main/java/com/asdf148/javaproject/global/config/JwtToken.java
+++ src/main/java/com/asdf148/javaproject/global/config/JwtToken.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
@@ -32,7 +33,7 @@ public class JwtToken {
                 .setExpiration(new Date(now.getTime() + Duration.ofMinutes(10).toMillis()))
                 .claim("id", user.getId().toString())
                 .claim("email", user.getEmail())
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode(secretKey.getBytes()))
                 .compact();
 
     }
@@ -47,13 +48,13 @@ public class JwtToken {
                 .setExpiration(new Date(now.getTime() + Duration.ofDays(14).toMillis()))
                 .claim("id", user.getId().toString())
                 .claim("email", user.getEmail())
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode(secretKey.getBytes()))
                 .compact();
     }
 
 
     public TokenContent decodeToken(String token) {
-        Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        Jws<Claims> claims = Jwts.parser().setSigningKey(Base64.getEncoder().encode(secretKey.getBytes())).parseClaimsJws(token);
 
         System.out.println("decodeToken: " + claims.getBody().get("id").toString());
         System.out.println(new ObjectId(claims.getBody().get("id").toString()));
