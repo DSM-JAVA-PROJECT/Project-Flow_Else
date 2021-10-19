c1ca8c2eb3355100e0b21dbd6652607f339967b4
asdf148 <wnduf0405_@naver.com>
2021-10-19 11:42:11 +0900

    :bug: :: Module name change
    Module name change

--- src/main/java/com/asdf148/javaproject/domain/member/controller/MemberController.java
+++ src/main/java/com/asdf148/javaproject/domain/member/controller/MemberController.java
@@ -2,12 +2,9 @@ package com.asdf148.javaproject.domain.member.controller;
 
 import com.asdf148.javaproject.domain.member.payload.ProjectMemberListResponse;
 import com.asdf148.javaproject.domain.member.service.MemberService;
-import com.asdf148.javaproject.global.config.JwtToken;
+import com.asdf148.javaproject.global.config.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,16 +12,16 @@ import java.security.Principal;
 public class MemberController {
 
     private final MemberService memberService;
-    private final JwtToken jwtToken;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/member")
     public ProjectMemberListResponse getMemberList(@RequestHeader("Authorization") String token, @PathVariable String projectId) {
-        return memberService.getMemberList(jwtToken.decodeToken(token.substring(7)).getEmail(), projectId);
+        return memberService.getMemberList(jwtUtil.decodeToken(token.substring(7)).getEmail(), projectId);
     }
 
     @GetMapping("/member/{chatRoomId}")
     public ProjectMemberListResponse getNotParticipatedMemberList(@RequestHeader("Authorization") String token, @PathVariable String projectId, @PathVariable String chatRoomId) {
-        return memberService.getNotParticipatedMemberList(jwtToken.decodeToken(token.substring(7)).getEmail(), projectId, chatRoomId);
+        return memberService.getNotParticipatedMemberList(jwtUtil.decodeToken(token.substring(7)).getEmail(), projectId, chatRoomId);
     }
 
 }
